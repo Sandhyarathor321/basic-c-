14a6fc5ff7fba9752a17182fecf2564b8370955a
Sandhyarathor321 <rathorsandhya301@gmail.com>
2024-10-04 18:07:43 +0530

    Add files via upload

---
+++ unary.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+class overLoading{
+int x;
+int y;
+
+public:
+overLoading(){ }
+overLoading(int x ,int y){ 
+this->x = x;
+this->y = y;
+}
+void operator ++(){
+    x+=1;
+    y+=1;
+}
+void operator ++(int noUse){
+    x+=1;
+    y+=1;
+}
+void operator --(){
+    x-=1;
+    y-=1;
+}
+void operator --(int noUse){
+    x-=1;
+    y-=1;
+}
+
+void show(){
+    cout<<x<<" "<<y;
+    cout<<endl;
+}
+
+};
+
+int main(){
+int n1,n2;
+printf("Enter two Number : ");
+scanf("%d %d",&n1,&n2);
+overLoading d1(n1,n2);
+++d1;
+printf("pre-increment  : ");
+d1.show();
+--d1;
+printf("pre-decrement  : ");
+d1.show();
+d1++;
+printf("post-increment  : ");
+d1.show();
+--d1;
+printf("post-decrement  : ");
+d1.show();
+
+}
